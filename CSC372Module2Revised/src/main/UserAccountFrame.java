868ff4c76c6572b34bc2e74ee7a827d2266f291f
Maynard Peralta <maynperalta@gmail.com>
2025-12-07 14:01:07 -0700

    Created methods for number formatting and hiding/showing elements.

--- CSC372Module2Revised/src/main/UserAccountFrame.java
+++ CSC372Module2Revised/src/main/UserAccountFrame.java
@@ -9,16 +9,16 @@ public class UserAccountFrame extends JFrame implements ActionListener {
     
 	private JLabel userBalanceLabel;
     private JFormattedTextField balanceField;
+    private JButton balanceBtn;
+    private JLabel confirmBalanceLabel;
     private JLabel userDepositLabel;
     private JFormattedTextField depositField;
+    private JButton depositBtn;
     private JLabel userWithdrawLabel;
     private JFormattedTextField withdrawField;
-    private JButton balanceBtn;
-    private JButton depositBtn;
     private JButton withdrawBtn;
     private double userBalance = 0;
-    private JLabel confirmBalanceLabel;
-	
+    
 	UserAccountFrame() {
         GridBagConstraints positionConst;
         	
@@ -40,24 +40,18 @@ public class UserAccountFrame extends JFrame implements ActionListener {
 // positioning for balance items               	
         positionConst.gridx = 1;
         positionConst.gridy = 0;
-        positionConst.fill = GridBagConstraints.NONE;
-        positionConst.weightx = 0;        
         add(userBalanceLabel, positionConst);
         
-        positionConst.gridx = 1;
         positionConst.gridy = 1;
         positionConst.fill = GridBagConstraints.HORIZONTAL;
         positionConst.weightx = 1.0;
         add(balanceField, positionConst);
         
-        positionConst.gridx = 1;
         positionConst.gridy = 2;
         positionConst.fill = GridBagConstraints.NONE;
         positionConst.weightx = 0;
         add(balanceBtn, positionConst);
-        
-        confirmBalanceLabel = new JLabel("");
-        confirmBalanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        confirmBalanceLabel = new JLabel("", SwingConstants.CENTER);
 // balance items in separate panel to avoid confines of frame grid        
         JPanel confirmPanel = new JPanel(new BorderLayout());
         confirmPanel.add(confirmBalanceLabel, BorderLayout.CENTER);
@@ -66,72 +60,50 @@ public class UserAccountFrame extends JFrame implements ActionListener {
         positionConst.gridy = 1;
         positionConst.gridwidth = 4;
         positionConst.fill = GridBagConstraints.HORIZONTAL;
-        positionConst.weightx = 1.0;       
         add(confirmPanel, positionConst);
 // reset grid width for other items        
         positionConst.gridwidth = 1;
  // deposit entry        
         userDepositLabel = new JLabel("Deposit Amount: ");
         depositField = new JFormattedTextField(format);
-        depositField.setEditable(true);
-        depositField.setText("");
         depositBtn = new JButton("Deposit");
         depositBtn.addActionListener(this);
         
         positionConst.gridx = 1;
         positionConst.gridy = 2;
-        positionConst.fill = GridBagConstraints.HORIZONTAL;
-        positionConst.weightx = 0;
         add(userDepositLabel, positionConst);
         
         positionConst.gridx = 2;
-        positionConst.gridy = 2;
         positionConst.fill = GridBagConstraints.HORIZONTAL;
-        positionConst.weightx = 1.0;
         add(depositField, positionConst);
         
         positionConst.gridx = 3;
-        positionConst.gridy = 2;
         positionConst.fill = GridBagConstraints.NONE;
-        positionConst.weightx = 0;
         add(depositBtn, positionConst);
 // withdrawal entry        
         userWithdrawLabel = new JLabel("Withdraw Amount: ");
         withdrawField = new JFormattedTextField(format);
-        withdrawField.setEditable(true);
-        withdrawField.setText("");
         withdrawBtn = new JButton("Withdraw");
         withdrawBtn.addActionListener(this);
         
         positionConst.gridx = 1;
         positionConst.gridy = 3;
-        positionConst.fill = GridBagConstraints.HORIZONTAL;
-        positionConst.weightx = 0;
         add(userWithdrawLabel, positionConst);
         
         positionConst.gridx = 2;
-        positionConst.gridy = 3;
         positionConst.fill = GridBagConstraints.HORIZONTAL;
-        positionConst.weightx = 1.0;
         add(withdrawField, positionConst);
         
         positionConst.gridx = 3;
-        positionConst.gridy = 3;
         positionConst.fill = GridBagConstraints.NONE;
-        positionConst.weightx = 0;
         add(withdrawBtn, positionConst);
 // hide deposit and withdrawal items until user enters balance        
-        userDepositLabel.setVisible(false);
-        depositField.setVisible(false);
-        depositBtn.setVisible(false);
-        userWithdrawLabel.setVisible(false);
-        withdrawField.setVisible(false);
-        withdrawBtn.setVisible(false);
+        viewTransactions(false);
+        
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 // set minimum size to ensure items are visible;
         setMinimumSize(new Dimension(400, 200));
-        setPreferredSize(new Dimension(400, 200));
 // allow frame to dynamically resize        
 		pack();
 		setVisible(true);
@@ -142,41 +114,49 @@ public class UserAccountFrame extends JFrame implements ActionListener {
 				JOptionPane.showMessageDialog(null,  String.format("Goodbye. Your balance is: $%.2f", userBalance));
 			}
 		});
-        }     
+    }
+	
+	private void viewTransactions(boolean visible) {
+		userDepositLabel.setVisible(visible);
+		depositField.setVisible(visible);
+		depositBtn.setVisible(visible);
+		userWithdrawLabel.setVisible(visible);
+		withdrawField.setVisible(visible);
+		withdrawBtn.setVisible(visible);
+			
+		revalidate();
+		repaint();
+		pack();
+	}
+	
+	private double formattedNumber(JFormattedTextField field) {
+		Number number = (Number) field.getValue();
+		if (number == null) throw new NumberFormatException();
+		return number.doubleValue();
+	}
 // button action event listeners
         @Override
         public void actionPerformed(ActionEvent e) {
         	try {
         		if (e.getSource() == balanceBtn) {
-        			userBalance = Double.parseDouble(balanceField.getText());
+        			userBalance = formattedNumber(balanceField);
         			confirmBalanceLabel.setText(String.format("Welcome. Your balance is: $%.2f", userBalance));
         			balanceField.setText("");
 // make deposit and withdrawal items visible after balance has been entered        			
-        			userDepositLabel.setVisible(true);
-        	        depositField.setVisible(true);
-        	        depositBtn.setVisible(true);
-        	        userWithdrawLabel.setVisible(true);
-        	        withdrawField.setVisible(true);
-        	        withdrawBtn.setVisible(true);
+        			viewTransactions(true);
 // hide balance items once entered        	        
         	        userBalanceLabel.setVisible(false);
         	        balanceField.setVisible(false);
         	        balanceBtn.setVisible(false);
-// refresh frame after updating visibility        	        
-        	        this.revalidate();
-        	        this.repaint();
-        	        pack();
-        	        
         		} else if (e.getSource() == depositBtn) {
-        			double amount = Double.parseDouble(depositField.getText());
+        			double amount = formattedNumber(depositField);
         			userBalance += amount;
         			confirmBalanceLabel.setText(String.format("Deposit Successful. New Balance: $%.2f", userBalance));
         			depositField.setText("");
         		} else if (e.getSource() == withdrawBtn) {
-        			double amount = Double.parseDouble(withdrawField.getText());
+        			double amount = formattedNumber(withdrawField);
         			if (amount > userBalance) {
         				JOptionPane.showMessageDialog(this, "Insufficient funds.", "Error", JOptionPane.ERROR_MESSAGE);
-        				withdrawField.setValue(null);
         				withdrawField.setText("");
         			} else {
         				userBalance -= amount;
